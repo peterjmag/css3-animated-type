80235e15f2326cab8f7d0618973a34ea7fad2d36
Peter Magenheimer <peter.magenheimer@gmail.com>
2011-09-02 22:12:58 -0600

    Randomize initial square positions.

--- js/script.js
+++ js/script.js
@@ -1,8 +1,13 @@
-/* Author:
+/* Author: Peter Magenheimer
 
 */
 
-
+$(function() {
+ $('.layout span').each(function() {
+   var rand = (Math.floor(Math.random()*8) + 1);
+   $(this).addClass('offscreen-' + rand);
+ });
+});
 
 
 
