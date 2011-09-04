893a8e042932cd931798e87be59aa223f918c18b
Peter Magenheimer <peter.magenheimer@gmail.com>
2011-09-03 21:45:36 -0600

    Add experimental hotkeys support.

--- js/script.js
+++ js/script.js
@@ -3,11 +3,17 @@
 */
 
 $(function() {
- $('.layout span').each(function() {
-   var rand = (Math.floor(Math.random()*17) + 1);
-   $(this).addClass('offscreen-' + rand);
- });
+  $('.layout span').each(function() {
+    var rand = (Math.floor(Math.random()*17) + 1);
+    $(this).addClass('offscreen-' + rand);
+  });
+  $(document).bind('keydown', 'a',function(evt){$('.a .legend')[0].focus()});
+  $(document).bind('keydown', 'b',function(evt){$('.b .legend')[0].focus()});
+  $(document).bind('keydown', 'c',function(evt){$('.c .legend')[0].focus()});
+  $(document).bind('keydown', 'd',function(evt){$('.d .legend')[0].focus()});
+  $(document).bind('keydown', 'e',function(evt){$('.e .legend')[0].focus()});
 });
+
 
 
 
