e56d76ba5e45a677b906000a7855524f2f27d214
Peter Magenheimer <peter.magenheimer@gmail.com>
2011-09-03 21:52:48 -0600

    Blur legend on hover.

--- js/script.js
+++ js/script.js
@@ -7,11 +7,12 @@ $(function() {
     var rand = (Math.floor(Math.random()*17) + 1);
     $(this).addClass('offscreen-' + rand);
   });
-  $(document).bind('keydown', 'a',function(evt){$('.a .legend')[0].focus()});
-  $(document).bind('keydown', 'b',function(evt){$('.b .legend')[0].focus()});
-  $(document).bind('keydown', 'c',function(evt){$('.c .legend')[0].focus()});
-  $(document).bind('keydown', 'd',function(evt){$('.d .legend')[0].focus()});
-  $(document).bind('keydown', 'e',function(evt){$('.e .legend')[0].focus()});
+  $(document).bind('keydown', 'a',function(evt){$('.a .legend').focus()});
+  $(document).bind('keydown', 'b',function(evt){$('.b .legend').focus()});
+  $(document).bind('keydown', 'c',function(evt){$('.c .legend').focus()});
+  $(document).bind('keydown', 'd',function(evt){$('.d .legend').focus()});
+  $(document).bind('keydown', 'e',function(evt){$('.e .legend').focus()});
+  $('.legend').mouseover(function(){$('.legend').blur()});
 });
 
 
